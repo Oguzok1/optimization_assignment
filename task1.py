97b0f6209857641735fabb4fda397153bce20e44
vlad <vladk6813@gmail.com>
2024-12-06 23:57:42 +0300

    fix subtask 1

--- task1.py
+++ task1.py
@@ -2,13 +2,6 @@ def f(x):
     return x**3 - 6*x**2 + 11*x - 6
 
 def bisection_method(f, a, b, epsilon):
-    if f(a) == 0:
-        return a
-    if f(b) == 0:
-        return b
-    if f(a) * f(b) > 0:
-        print("Bisection method fails. f(a) and f(b) must have opposite signs.")
-        return None
     c = (a + b) / 2.0
     while abs(f(c)) >= epsilon:
         if f(a) * f(c) < 0:
